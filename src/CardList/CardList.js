1c17578daf559f5310af0419573261e26d648bc2
girish82 <pgirish4u@gmail.com>
2020-11-11 11:50:36 +0100

    Deployment Commit

--- src/CardList/CardList.js
+++ src/CardList/CardList.js
@@ -5,13 +5,23 @@ import './CardList.css'
 import { NavLink } from 'react-router-dom'
 
 const CardList = (props) => {
+    function convertDate(inputFormat) {
+        function pad(s) { return (s < 10) ? '0' + s : s; }
+        var d = new Date(inputFormat)
+        // console.log(inputFormat,d)
+        // console.log([d.getFullYear(), pad(d.getMonth()+1),pad(d.getDate())].join('-'))
+        return [pad(d.getDate()),pad(d.getMonth()+1),d.getFullYear()].join('/')
+      }
+
     const data = () => {
     return (
         <div>
             {Object.keys(props.details).map((key, index) => {
                  return (<div className="cardBlock" key={key}>
                 <Card.Title className="cardTitle">{key}</Card.Title>
-                <Card.Text className="cardText">{props.details[key]}</Card.Text>
+                <Card.Text className="cardText">
+                    {key === "from" || key === "to" ? convertDate(props.details[key]) : props.details[key]}
+                </Card.Text>
                 </div>)
             })}
         </div>
